1daade11e715a445f778be91944f0a4f70fd0d6b
Waribu George <gwaribu@gmail.com>
2022-06-26 00:47:01 +0300

    final commit of volume one

--- Js/BudgetTracker.js
+++ Js/BudgetTracker.js
@@ -82,25 +82,68 @@ export default class BudgetTracker {
     this.updateSummary();
   }
 
-  updateSummary() {}
+  updateSummary() {
+    const total = this.getEntryRows().reduce((total, row) => {
+      const amount = row.querySelector(".input-amount").value;
+      const isExpense = row.querySelector(".input-type").value === "expense";
+      const modifier = isExpense ? -1 : 1;
 
-  save() {}
+      return total + amount * modifier;
+    }, 0);
+
+    const totalFormatted = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "KSH",
+    }).format(total);
+
+    this.root.querySelector(".total").textContent = totalFormatted;
+  }
+
+  save() {
+    const data = this.getEntryRows().map((row) => {
+      return {
+        date: row.querySelector(".input-date").value,
+        description: row.querySelector(".input-description").value,
+        type: row.querySelector(".input-type").value,
+        amount: parseFloat(row.querySelector(".input-amount").value),
+      };
+    });
+
+    localStorage.setItem("budget-tracker-entries", JSON.stringify(data));
+    this.updateSummary();
+  }
 
   addEntry(entry = {}) {
-        this.root
+    this.root
       .querySelector(".entries")
       .insertAdjacentHTML("beforeend", BudgetTracker.entryHtml());
 
-      const row = this.root.querySelector(".entries tr:last-of-type");
-      
-      row.querySelector(".input-date").value = entry.date || new Date().toISOString().replace(/T.*/, "");
+    const row = this.root.querySelector(".entries tr:last-of-type");
+
+    row.querySelector(".input-date").value =
+      entry.date || new Date().toISOString().replace(/T.*/, "");
+    row.querySelector(".input-description").value = entry.description || "";
+    row.querySelector(".input-type").value = entry.type || "income";
+    row.querySelector(".input-amount").value = entry.amount || 0;
+    row.querySelector(".delete-entry").addEventListener("click", (e) => {
+      this.onDeleteEntryBtnclick(e);
+    });
+
+    row.querySelectorAll(".input").forEach((input) => {
+      input.addEventListener("change", () => this.save());
+    });
   }
 
-  getEntryRows() {}
+  getEntryRows() {
+    return Array.from(this.root.querySelectorAll(".entries tr"));
+  }
 
   onNewEntryBtnClick() {
     this.addEntry();
   }
 
-  onDeleteEntryBtnclick() {}
+  onDeleteEntryBtnclick(e) {
+    e.target.closest("tr").remove();
+    this.save();
+  }
 }
